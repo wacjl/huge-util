828cb7cef9344ee4e0063578af14728b742052b6
zhoudj <zhoudj@ersoft.cn>
2019-07-11 11:48:56 +0800

    email

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 })(this, function() {
   "use strict";
   const phoneReg = /^1\d{10}$/;
+  const emailRge = /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/;
   return {
-    phoneReg
+    phoneReg,
+    emailRge
   };
 });
